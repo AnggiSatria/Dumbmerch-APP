d11605946642b7cfb9bebfea174d0f6003ba9b92
AnggiSatria <anggisatria122@gmail.com>
2022-06-10 23:41:46 +0700

    v11

--- client/src/components/registers.jsx
+++ client/src/components/registers.jsx
@@ -194,7 +194,7 @@ function Registers(){
                     </div>
 
                 
-                        <Button onClick={handleAlert} variant="danger" style={{borderRadius : '5px',backgroundColor : 'red', marginTop : '15px', marginBottom : '30px',width : '90%',textDecoration : 'none', color : 'white', textAlign : 'center'}}>Register</Button>
+                        <Button onClick={handleAlert} variant="danger" style={{borderRadius : '5px', marginTop : '15px', marginBottom : '30px',width : '90%',textDecoration : 'none', color : 'white', textAlign : 'center'}}>Register</Button>
                 </div>
                 
             </div>
